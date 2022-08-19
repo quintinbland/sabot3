0a815cacd411eb13561ee9fa71d493311ab24fd7
Martin Smith <msmith92663@gmail.com>
2022-08-18 21:37:20 -0700

    plan is evolving

--- Proposal.md
+++ Proposal.md
@@ -43,7 +43,7 @@ vault vs dao - dao implies governance, vault is like a bank
 
 Scenario
 * The five of us like sabot, we want to form a collective that leverages sabot for automated arbitrage trading
-* In the hypothetical, we invest $20K for a total of $100k
+* In the hypothetical, we invest $20K for a total of $100k + $15K for bot gas wallet - (de centralized bot that manages gas wallet - gelato)
 * 5 members, but we don't trust each other
 * smart contract hold our funds, represent ownership stake via token
 * the wallet that creates the contract is the owner of the contract
@@ -78,10 +78,6 @@ Multi-sig (gnosis wallet)
    * get current value of sabot token
    * get history of bot
 
-intentionally out of scope
-* selling sabot tokens
-* distribution of funds
-
 
 
 ### Activities
@@ -91,10 +87,10 @@ intentionally out of scope
 * cron and random trade direction for smart contract and calls smart contract (John)
 * Prototype 0X filter for curve (Kevin - Priority B)
 * Test contract call curve contract to do a trade, where is curve on the test chain?
-    * find Curve / USDT / SUSD on test chain
+    * find Curve / USDT / SUSD on test chain (Yanick)
     * mock curve?
 ----- smart contract    
-* Clog Token
+* Clog Token (Quintin)
 * Smart contract exchange USDT for clog token
     * minting
 * Smart contract to execute trades with curve on chain
@@ -103,10 +99,17 @@ intentionally out of scope
       library
       static contract address
       (open zeppeling proxy)
+   * prototype the contract calling another contract (Kevin / Yannick)
+       * contract 1 - called by a web3 client   , it has two methods, method 1 and method 2
+       * contract 2 - called by contract 1 - method 2
+       * Is the gas fee calling contract 1 menthod 1 cheaper than calling contract 1 method 2 (which is the one calling contract 2)?
+       * Does it work or is there an out of gas error.
+   * Stake (mint) / Unstake (burn)  (John)
+   * trade method  (Martin)
 ------ 
 * Streamlit UI (MVP)
     * web3 like experience (using your web3 wallet) - exchange usdt for token (Quintin)
     * Graph of balances ( or other)
     * view filtered contract events - insight of what is happenning on-chain
-        * research getting events in 
+        * research getting events in (Yanick)
 * Metamask integration - see token balance
---
+++ sabotcontract.sol
@@ -0,0 +1,113 @@
+pragma solidity ^0.6.12;
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+contract SaBot_DAO {
+    // call it DefiBank
+    string public name = "SaBot DAO";
+    // create 3 state variables
+    address public usdt;
+    address public susd;
+    address public clog;
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+    // in constructor pass in the address for USDT token and your custom bank token
+    // that will be used to pay interest
+    constructor() public {
+       // CLOG = INPUT-YOUR-TOKEN-ADDRESS-HERE;
+       // USDT =
+       // SUSD =
+    }
+    // allow user to stake usdc tokens in contract
+    function stakeTokens(uint _amount) public {
+        // Trasnfer usdc tokens to contract for staking
+        IERC20(USDT).transferFrom(msg.sender, address(this), _amount);
+        // REQUIRE USER HAS ENOUGH USDT
+        // Update the staking balance in map
+        //stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+        //MINT CLOG TOKEN BASED ON VALUE OF WHAT HAS BEEN STAKED
+        // Initially on a 1:1 ratio, but then make it relate to the value of user's tokens in relation to total pool amount
+        // The bot has a total portfolio value. The clog value is the outstanding supply of tokens divided by total portfolio value. Mint when we stake. Burn when we unstake.
+        // if outstanding balance of CLOG is 0, then CLOG price = 1 . If outstanding supply of clog >0 then price of clog = portfolio value/outstanding CLOG supply
+        //Outstanding supply | Portfolio Value | CLOG price
+        //        0          |         0       |      1.00
+        //      20,000       |     20,000USDT  |      1.00
+        //      40,000       |     40,000USDT  |      1.00
+        //     100,000       |    100,000USDT  |      1.00
+        // After 1 year, and profitable trades
+        //     100,000       |    300,000USDT  |      3.00
+        // Other people enter the pool at this point
+        //     150,000       |    450,000USDT  |      3.00
+        //Opportunity for governance token that takes 3bps of all profit when unstaking.
+        // Add user to stakers array if they haven't staked already
+        //if(!hasStaked[msg.sender]) {
+          //  stakers.push(msg.sender);
+        //}
+        // Update staking status to track
+        //isStaking[msg.sender] = true;
+        //hasStaked[msg.sender] = true;
+        //EMIT MINTING EVENT
+        //EMIT STAKING EVENT
+    }
+        // allow user to unstake total balance and withdraw USDC from the contract
+     function unstakeTokens(uint _amount) public {
+        // REQUIRE USER HAS ENOUGH CLOG
+        // Trasnfer usdc tokens to contract for staking
+        IERC20(CLOG).transferFrom(msg.sender, address(this), _amount);
+        //If outstanding supply of clog >0 then price of clog = portfolio value/outstanding CLOG supply
+        //Transfer CLOG price X CLOG amount in USDT to message.sender.  Make sure we have enough USDT to cover staking.
+        // If we don't have enough USDT, we error out and tell them what is available to unstake.
+        //BURN CLOG TOKEN BASED ON VALUE OF WHAT HAS BEEN STAKED
+        // Initially on a 1:1 ratio, but then make it relate to the value of user's tokens in relation to total pool amount
+        // The bot has a total portfolio value. The clog value is the outstanding supply of tokens divided by total portfolio value. Mint when we stake. Burn when we unstake.
+        // if outstanding balance of CLOG is 0, then CLOG price = 1 . If outstanding supply of clog >0 then price of clog = portfolio value/outstanding CLOG supply
+        //Outstanding supply | Portfolio Value | CLOG price
+        //        0          |         0       |      1.00
+        //      20,000       |     20,000USDT  |      1.00
+        //      40,000       |     40,000USDT  |      1.00
+        //     100,000       |    100,000USDT  |      1.00
+        // After 1 year, and profitable trades
+        //     100,000       |    300,000USDT  |      3.00
+        // Other people enter the pool at this point
+        //     150,000       |    450,000USDT  |      3.00
+        //Opportunity for governance token that takes 3bps of all profit when unstaking.
+        //EMIT BURN EVENT
+        //EMIT UNSTAKING EVENT
+    }
+}
+
+// helper methons
+//function getClogPrice () public
+ //   returns (UFixed256x18) {}
+
+// function Need Outstanding supply
+// function Get Portfolio Value
+
+// NEXT TOPIC ----------------------------------------
+//SaBot trader section
+// function swap(sellToken address, sellToken amount, buyToken address)
+//    a sabot buy event is when sellToken is USDT
+//    a sabot sell event is when sellToken is sUSD
+
+//    This function will 
+//      * check that there is enough sellToken balance available to do the swap
+//      * call curve to execute the swap (is the gas fee to call the curve function(s) covered by the gas fee used to call this function?)
+//      *   let's prototype this to determine how gas will be handled
+//      * emit a swap event
+
+// helper method
+// get_balance(currency_address) // to get balance of USDT and sUSD
+
