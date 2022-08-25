fb800b32c8c577f1cfc9f13dab5d1c8b30667d25
johngru <john.h.gruenewald@gmail.com>
2022-08-24 18:40:41 -0700

    Wallet generator files

--- crypto_wallet.py
+++ crypto_wallet.py
@@ -1,26 +1,95 @@
-# Cryptocurrency Wallet
+# BIP-44 Wallet Creation
 ################################################################################
 
-# This file contains the Ethereum transaction functions that you have created throughout this module’s lessons. By using import statements, you will integrate this `crypto_wallet.py` Python script into the Fintech Finder interface program that is found in the `fintech_finder.py` file.
+# Implements the BIP-44 standards for multicurrency and multiaccount HD wallets.
+# Store mnemonic in an .env file and a master account will be created implementing
+# BIP-44. An arbitrary number (up to 2^{31} -1) of normal children addresses can be
+# generated from parent address.
 
 ################################################################################
 # Imports
 import os
 import requests
 from dotenv import load_dotenv
-load_dotenv()
 from bip44 import Wallet
+from mnemonic import Mnemonic
+
+
 from web3 import Account
 from web3 import middleware
 from web3.gas_strategies.time_based import medium_gas_price_strategy
+from web3 import Web3
+
+################################################################################
+# Connect to network
+def connect_RPC_server(rpc_server_address):
+    """
+    Connect to the blockchain network.
+    
+    Input Parameters:
+    rpc_server_address (str) : Address of RPC Server, i.e. 'HTTP://127.0.0.1:7545'
+                               for Ganache
+    
+    Returns:
+    w3 (obj): An instance of Web3 for the inputed network
+    """
+    return Web3(Web3.HTTPProvider(rpc_server_address))
+
+################################################################################
+# Finding Mnemo
+def mnemonic_generator(strength=128,
+                       language='english'):
+    """
+    Checks .env file of the parent directory for variable named mnemonic ("MNEMONIC").
+    If file is empty, mnemonic is created with specified strength (128-bit or 256-bit).
+    Stores newly created mnemonic phrase in .env file.
+    
+    Input Parameters:
+    
+    strength (int) : Strength of seed mnemonic phrase implementing the 2,048 word
+                     list of BIP-39.
+                     Default strength is equal to 128.
+                     For 12-word phrases, use 128.  For 24-word phrases, use 256.  
+    language (str) : Default language is 'english'.  For complete list of options,
+                     visit: https://github.com/bitcoin/bips/tree/master/bip-0039
+    
+    Returns:
+    
+    mnemonic (str) : A string with the BIP-39 mnemonic of specified strength.
+    
+    """
+    load_dotenv()
+    mnemonic = os.getenv("MNEMONIC")
+    
+    check_sum = {128:4,
+                256:8}
+    # Use an if-statement to check if the mnemonic variable is None
+    if((mnemonic == None) | (mnemonic=='')):
+        mnemo = Mnemonic(language)
+    
+        mnemonic = mnemo.generate(strength)
+        
+        with open('.env','a') as f:
+            f.write(f'MNEMONIC={mnemonic}')
+        
+        return mnemonic
+    elif((11.0*len(mnemonic.split())-check_sum[strength])/strength != 1):
+        return '''Provided MNEMONIC in .env file does not match specified strength. Check word count or strength(128 or 256).'''        
+    else:
+        return mnemonic
+
 
 ################################################################################
 # Wallet functionality
 
-def generate_account():
+###  N.B.> generate account, generate i^th index account, generate 0 thru i accounts.
+
+def generate_account(strength=256,
+                    language='english',
+                    ):
     """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
     # Fetch mnemonic from environment variable.
-    mnemonic = os.getenv("MNEMONIC")
+    mnemonic = mnemonic_generator()
 
     # Create Wallet Object
     wallet = Wallet(mnemonic)
