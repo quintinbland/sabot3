96a5fd5cf92b3a359bfaf5c122d7dba6882925e0
Martin Smith <msmith92663@gmail.com>
2022-08-26 14:40:46 -0700

    updating event filter

--- event_filter.py
+++ event_filter.py
@@ -3,35 +3,51 @@ w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 import time
 import json
 from pathlib import Path
-# from crypto_wallet import generate_account, get_balance,send_transaction
+from datetime import datetime
+import csv
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+writer = None
 
 def handle_event(contract,event_raw):
-    # SabotSwap event defines args sellToken, sellTokenAmount, buyToken, buyTokenAmount
+    global writer
     receipt = w3.eth.getTransactionReceipt(event_raw['transactionHash'])
     # SabotSwap event defines args sellToken, sellTokenAmount, buyToken, buyTokenAmount
     swap_event = contract.events.SabotSwap().processReceipt(receipt)[0]  
-    # Minted eventSabotSwap event defines clogAmount, owner
+    # Minted event defines clogAmount, owner
     minted_event = contract.events.Minted().processReceipt(receipt)[0]  
     # Staking event defines args owner, sellToken, sellTokenAmount, clogAmount
     staking_event = contract.events.Staking().processReceipt(receipt)[0]  
+    event_record = {
+        'time':datetime.now().timestamp(),
+        'type':None,
+        'sellToken':None,
+        'sellTokenAmount':None,
+        'buyToken':None,
+        'buyTokenAmount':None,
+        'clogAmount':None,
+        'owner':None
+    }
     if swap_event is not None and 'event' in swap_event:
-        args = f"event: {swap_event['event']}\n"
+        event_record['type']='SabotSwap'
         if 'args' in swap_event:
             for key,value in swap_event['args'].items():
-                args += f"\t{key}={value}\n"
-        print(args)
-    if minting_event is not None and 'event' in minting_event:
-        args = f"event: {minting_event['event']}\n"
-        if 'args' in minting_event:
-            for key,value in minting_event['args'].items():
-                args += f"\t{key}={value}\n"
-        print(args)
+                event_record[key]=value
+    if minted_event is not None and 'event' in minted_event:
+        event_record['type']='Minter'
+        if 'args' in minted_event:
+            for key,value in minted_event['args'].items():
+                event_record[key]=value
     if staking_event is not None and 'event' in staking_event:
-        args = f"event: {staking_event['event']}\n"
+        event_record['type']='Staking'
         if 'args' in staking_event:
             for key,value in staking_event['args'].items():
-                args += f"\t{key}={value}\n"
-        print(args)
+                event_record[key]=value
+    print(event_record)
+    if writer is not None:
+        writer.writerow(event_record)     
 
 def log_loop(contract,contract_filter, poll_interval):
     while True:
@@ -43,6 +59,23 @@ def log_loop(contract,contract_filter, poll_interval):
         time.sleep(poll_interval)
 
 def main():
+    global writer
+    event_record = {
+        'time':datetime.now().timestamp(),
+        'type':None,
+        'sellToken':None,
+        'sellTokenAmount':None,
+        'buyToken':None,
+        'buyTokenAmount':None,
+        'clogAmount':None,
+        'owner':None
+    }
+
+    event_log_path = Path(os.environ['EVENT_LOG'])
+    if not event_log_path.exists():
+        with open(event_log_path, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=event_record.keys())
+            writer.writeheader()    
     # block_filter = w3.eth.filter('latest')
     abi=None
     with open(Path('SabotSwapBase_abi.json'),'r') as abi_file:
@@ -51,7 +84,9 @@ def main():
    
     contract = w3.eth.contract(address = contract_address , abi = abi)    
     contract_filter = w3.eth.filter({"address": contract_address})
-    log_loop(contract,contract_filter, 2)
+    with open(event_log_path, 'a') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=event_record.keys())
+        log_loop(contract,contract_filter, 2)
 
 if __name__ == '__main__':
     main()
